702be4d8d8fc9678e906b3d0b39661d7c2356f49
PoornimaPalliyaGuruge <86935934+PoornimaPalliyaGuruge@users.noreply.github.com>
2023-03-22 22:28:03 +0530

    sales design done

--- app/src/main/java/com/example/mad_business_android_app/MainActivity.kt
+++ app/src/main/java/com/example/mad_business_android_app/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sales_view)
+        setContentView(R.layout.splash_screen)
 
 
     }
