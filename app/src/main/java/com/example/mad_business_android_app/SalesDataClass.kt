b5b51103559ab76669b22a8dfc4068938fdbd398
PoornimaPalliyaGuruge <86935934+PoornimaPalliyaGuruge@users.noreply.github.com>
2023-05-05 22:50:44 +0530

    Sales view data class

--- app/src/main/java/com/example/mad_business_android_app/SalesDataClass.kt
+++ app/src/main/java/com/example/mad_business_android_app/SalesDataClass.kt
@@ -2,15 +2,22 @@ package com.example.mad_business_android_app
 
 class SalesDataClass {
 
+    var salesID :String? = null
     var cusID : String? = null
     var productName : String? = null
-    var quentity : String? = null
+    var quantity : String? = null
     var total : String? = null
 
-    constructor(cusID: String?, productName: String?, quentity: String?, total: String?) {
+
+    constructor(salesID:String?,cusID: String?, productName: String?, quantity: String?, total: String?) {
+        this.salesID = salesID
         this.cusID = cusID
         this.productName = productName
-        this.quentity = quentity
+        this.quantity = quantity
         this.total = total
+    }
+
+    constructor(){
+
     }
 }
