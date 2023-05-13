15b384c7bce05cfa25cee47a9f2de7e02ac41724
PoornimaPalliyaGuruge <86935934+PoornimaPalliyaGuruge@users.noreply.github.com>
2023-05-13 12:24:01 +0530

    Add total sales

--- app/src/main/java/com/example/mad_business_android_app/ActSalesHome.kt
+++ app/src/main/java/com/example/mad_business_android_app/ActSalesHome.kt
@@ -3,24 +3,46 @@ package com.example.mad_business_android_app
 import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.InputBinding
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mad_business_android_app.databinding.ActivitySalesBinding
+import com.google.firebase.database.*
 
 class ActSalesHome : AppCompatActivity() {
 
     private lateinit var binding: ActivitySalesBinding
-
+    private lateinit var tv_sh_total:TextView
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySalesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        tv_sh_total = findViewById(R.id.tv_sh_total)
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Sales Add")
 
-       binding.salesAddFab.setOnClickListener {
-           val intent = Intent(this,AddSalesActivity::class.java)
-           startActivity(intent)
-       }
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var sum = 0
+                dataSnapshot.children.forEach { ds ->
+                    val map = ds.value as Map<String, Any>
+                    val total = map["total"]
+                    val tValue = total.toString().toInt()
+                    sum += tValue
+                }
+                tv_sh_total.text = sum.toString()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // handle error here
+            }
+        })
+
+        binding.salesAddFab.setOnClickListener {
+            val intent = Intent(this,AddSalesActivity::class.java)
+            startActivity(intent)
+        }
 
         binding.salesViewFab.setOnClickListener {
             val intent = Intent(this,ActSalesView::class.java)
@@ -30,7 +52,5 @@ class ActSalesHome : AppCompatActivity() {
             val intent = Intent(this,FetchingSalesData::class.java)
             startActivity(intent)
         }
-
     }
-
 }
--- app/src/main/java/com/example/mad_business_android_app/MainActivity.kt
+++ app/src/main/java/com/example/mad_business_android_app/MainActivity.kt
@@ -3,9 +3,15 @@ package com.example.mad_business_android_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.mad_business_android_app.databinding.HometabsBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.FirebaseApp
 
 
